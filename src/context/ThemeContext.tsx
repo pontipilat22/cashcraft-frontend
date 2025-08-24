26e5f847f40a2779a45719e9146f4728739864d4
Evgeny Zarutskiy <evgeny@example.com>
2025-08-24 16:57:58 +0600

    first commit55

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -18,6 +18,7 @@ interface ThemeContextType {
     statsBg: string;
     warning: string;
     danger: string;
+    error: string;
     success: string;
     successLight: string;
     dangerLight: string;
@@ -47,6 +48,7 @@ const lightTheme = {
   statsBg: '#4287f5',
   warning: '#ff9800',
   danger: '#f44336',
+  error: '#f44336',
   success: '#4CAF50',
   successLight: '#E8F5E9',
   dangerLight: '#FFEBEE',
@@ -75,6 +77,7 @@ const darkTheme = {
   statsBg: 'rgba(255, 104, 0, 0.9)',
   warning: '#ff9800',
   danger: '#f44336',
+  error: '#f44336',
   success: '#4CAF50',
   successLight: '#1B5E20',
   dangerLight: '#B71C1C',
